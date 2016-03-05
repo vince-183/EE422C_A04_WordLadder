7c085ccc0476f9157fac9c2bdf57f1f09a386fc1
Vincent Ip <vincent.ip@utexas.edu>
2016-03-04 21:19:30 -0600

    Code has been cleaned up. computeLadder has been broken up into smaller, more manageable private methods which are called by computeLadder. Comments have been added for each method. Exceptions have been added - illegal word input and noSuchLadder have been implemented. Next: figure out JUnit, add documentation

--- src/Assignment4/Assign4Driver.java
+++ src/Assignment4/Assign4Driver.java
@@ -8,20 +8,22 @@ import java.util.*;
 public class Assign4Driver
 {
     public static void main(String[] args)
-    {
-       // Create a dictionary ArrayList
-   	 
+    { 
    	 // Create a word ladder solver object
         Assignment4Interface wordLadderSolver = new WordLadderSolver();
 
         try 
         {
-           
-      	  List<String> result = wordLadderSolver.computeLadder("hello","world", -1);
-      	  int i = 0;
-      	  int j = i - 3;
-      	  System.out.printf("%d", j);
-      	  //boolean correct = wordLadderSolver.validateResult("money", "honey", result);
+      	  // Compute a word ladder between two words if one exists
+      	  List<String> result = wordLadderSolver.computeLadder("hemps","panda", -1);
+      	  
+      	  // Print out the word ladder if it exists
+      	  Iterator<String> resultIter = result.iterator();
+      	  while (resultIter.hasNext())
+      	  {
+      		  System.out.print(resultIter.next() + " ");
+      	  }
+      	  System.out.println("\nTotal words: " + result.size());
         }
         catch (NoSuchLadderException e) 
         {
--- src/Assignment4/WordLadderSolver.java
+++ src/Assignment4/WordLadderSolver.java
@@ -1,5 +1,10 @@
-/*
-    ADD YOUR HEADER HERE
+/**
+    WordLadderSolver
+    
+    Written by Jonathan Friesen and Vincent Ip
+    EE422C Assignment 4
+    Spring 2016
+    The University of Texas at Austin
  */
 
 package Assignment4;
@@ -12,15 +17,20 @@ import java.util.*;
 public class WordLadderSolver implements Assignment4Interface
 {
     // declare class members here.
-	private Scanner reader = null;
+	private Scanner reader = null; // used to read the dictionary file to populate the dictionary.
 	protected List<String> SolutionList = new ArrayList<String>(); // This is the solutionList to be returned and printed if successful
-	protected List<String> dictionary = new ArrayList<String>();
+	protected List<String> dictionary = new ArrayList<String>(); // The dictionary to populate and use.
 	protected List<String> checkedList = new ArrayList<String>(); // words we have checked so far
 	
     // add a constructor for this object. HINT: it would be a good idea to set up the dictionary there
+	/**
+	 * Constructor
+	 * Builds the 5-letter-word dictionary against which candidate words are checked.
+	 * The dictionary is an ArrayList of Strings.
+	 */
 	public WordLadderSolver()
 	{
-		// Populate dictionary against which to check legality of words
+		// Populate the dictionary against which to check legality of words
 		try 
       {
          reader = new Scanner(new FileInputStream("A4words.dat"));
@@ -31,7 +41,7 @@ public class WordLadderSolver implements Assignment4Interface
          reader.close();
          for (int i = 0; i < 4; i++)
          {
-       	  dictionary.remove(0); // get rid of first 4 lines of crap
+       	  dictionary.remove(0); // get rid of the first 4 lines of crap
          }
          for (int j = 0; j < dictionary.size(); j++)
          {
@@ -44,64 +54,133 @@ public class WordLadderSolver implements Assignment4Interface
     	  System.out.println("File not found!");
       }
 	}
+	
+	/**
+	 * checkValidInput
+	 * 
+	 * This is a private method called ONLY by computeLadder.
+	 * Checks if the startWord and endWord are legal inputs. Both words must be:
+	 * 1) 5 letters long.
+	 * 2) a legal dictionary word.
+	 * If not, the appropriate exception is thrown.
+	 * @param startWord
+	 * @param endWord
+	 */
+	private void checkValidInput(String startWord, String endWord)
+	{
+		if (startWord.length() != 5 || endWord.length() != 5)
+		{
+			throw new IllegalArgumentException("Invalid word length! Please use 5-letter words.");
+		}
+		if (!(dictionary.contains(startWord)))
+		{
+			throw new IllegalArgumentException("Start word is not a legal dictionary word!");
+  	 	}
+  	 	if (!(dictionary.contains(endWord)))
+  	 	{
+  	 		throw new IllegalArgumentException("End word is not a legal dictionary word!");
+  	 	}
+	}
+	
+	/**
+	 * generateTempList
+	 * 
+	 * This is a private method called ONLY by computeLadder. This method generates
+	 * a list of words that differ from startWord by 1 letter, in a position other than the
+	 * value of pos.
+	 * @param startWord
+	 * @param pos
+	 * @return tempList - the temporary list of candidate words
+	 */
+	private List<String> generateTempList(String startWord, int pos)
+	{
+		List<String> tempList = new ArrayList<String>();
+		int diffPos = 0;
+      Iterator<String> dictionIter = dictionary.iterator();
+      while (dictionIter.hasNext())
+      {
+
+    	  String dicTerm = dictionIter.next();
+    	  int diffCounter = 0;
+    	  int index = 0;
+    	  while (index < 5)
+    	  {
+    		  // Go through the words letter-by-letter and compare the letters.
+    		  // If different, increment a difference counter.
+    		  if (dicTerm.charAt(index) != startWord.charAt(index))
+    		  {
+    			  diffCounter++;
+    			  diffPos = index;
+    		  }
+    		  index++;
+    	  }
+    	  if ((diffCounter == 1) && (diffPos != pos) && !(SolutionList.contains(dicTerm)) && !(checkedList.contains(dicTerm)))
+    	  {
+    		  // add the candidate word to the tempList IF AND ONLY IF the the word differs from startWord by one letter
+    		  // in a position different than pos, AND the current solution list does NOT contain the candidate word.
+    		  tempList.add(dicTerm);
+    		  tempList.set(tempList.indexOf(dicTerm), dicTerm + diffPos);
+    	  }
+      }
+      return tempList;
+	}
+	
+	/**
+	 * computeDiff
+	 * 
+	 * This is a private method called ONLY by computeLadder. Computes the number
+	 * of letter positions that are different between wordA and wordB.
+	 * The result is an integer.
+	 * @param wordA
+	 * @param wordB
+	 * @return int diffCounter
+	 */
+	private int computeDiff(String wordA, String wordB)
+	{
+		int diffCounter = 0;
+ 	  	for (int i = 0; i < 5; i++)
+ 	  	{
+ 	  		if (wordA.charAt(i) != wordB.charAt(i))
+ 	  		{
+ 	  			diffCounter++;
+ 	  		}
+ 	  	}
+ 	  	return diffCounter;
+	}
 
     // do not change signature of the method implemented from the interface
-    @Override
+    /**
+     * ************************* computeLadder ******************************
+     * This is a recursive implementation of a method that computes a word ladder between two legal 5-letter
+     * words.
+     * @param startWord	(5 letters)
+     * @param endWord	(5 letters)
+     * @param pos			(last letter position changed)
+     * 
+     * @return ArrayList<String> containing the "word ladder" that exists between startWord and endWord
+     */
+	@Override
     public List<String> computeLadder(String startWord, String endWord, int pos) throws NoSuchLadderException 
     {
-   	 if (startWord.length() != 5 && endWord.length() != 5)
-   	 {
-   		 // invalid input
-   	 }
-   	  SolutionList.add(startWord);
-        if (startWord.equals(endWord))
+   	 checkValidInput(startWord, endWord); // Throw exception if input is invalid.
+   	 SolutionList.add(startWord);
+        if (startWord.equals(endWord)) // start Word is the same as end Word; return start Word as the solution
         {
       	  return SolutionList;
         }
-        if(checkedList.contains(startWord)){// if we have already checked this word and it did not give us word ladder, return up
-        	SolutionList.remove(startWord);
-        	return SolutionList;
+        if(checkedList.contains(startWord)) // if we have already checked this word and it did not give us word ladder, return up
+        {
+      	  SolutionList.remove(startWord);
+      	  return SolutionList;
         }
         
-        List<String> tempList = new ArrayList<String>();
-        //Iterator<String> tempIter = tempList.iterator();
-        // Search dictionary for words that differ from startWord by 1 letter
-        int diffPos = 0;
-        Iterator<String> dictionIter = dictionary.iterator();
-        while (dictionIter.hasNext())
-        {
-
-      	  String dicTerm = dictionIter.next();
-      	  int diffCounter = 0;
-      	  int index = 0;
-      	  while (index < 5 && diffCounter < 2)
-      	  {
-      		  if (dicTerm.charAt(index) != startWord.charAt(index))
-      		  {
-      			  diffCounter++;
-      			  diffPos = index;
-      		  }
-      		  index++;
-      	  }
-      	  if ((diffCounter == 1) && (diffPos != pos) && !(SolutionList.contains(dicTerm)) && !(checkedList.contains(dicTerm)))
-      	  {
-      		  // add the candidate word to the tempList IF AND ONLY IF the the word differs from startWord by one letter
-      		  // in a position different than pos, AND the current solution list does NOT contain the candidate word.
-      		  tempList.add(dicTerm);
-      		  tempList.set(tempList.indexOf(dicTerm), dicTerm + diffPos);
-      	  }
-        }
+        // Generate tempList (list of candidate words that differ from startWord by 1 letter)
+        List<String> tempList = generateTempList(startWord, pos);
+        
         for (int j = 0; j < tempList.size(); j++) // Go through the tempList and find the diffCounter vs. endWord
         {
-      	  int diffCounter = 0;
-           int index = 0;
-      	  for (int i = 0; i < 5; i++)
-      	  {
-      		  if (tempList.get(j).charAt(i) != endWord.charAt(i))
-         	  {
-         		  diffCounter++;
-         	  }
-      	  }
+      	  // Get the NUMBER of letter positions that DIFFER from the endWord.
+      	  int diffCounter = computeDiff(tempList.get(j), endWord);
       	  
       	  tempList.set(j, diffCounter + tempList.get(j)); // Prepend candidate word with the difference counter
       	  
@@ -112,20 +191,23 @@ public class WordLadderSolver implements Assignment4Interface
       		  return SolutionList;
       	  }
         }
-        Collections.sort(tempList); // Sort the tempList by number of positions DIFFERENT from endWord
-        for (int j = 0; j < tempList.size(); j++)
+        Collections.sort(tempList); // Sort the tempList by number of letter positions DIFFERENT from endWord
+        for (int j = 0; j < tempList.size(); j++) // Go through the sorted tempList and find possible word ladders for each word in the list
         {
       	  int newPos = Integer.parseInt(tempList.get(j).substring(6));
       	  tempList.set(j, tempList.get(j).substring(1,6)); // exclude the prepended and appended numbers
-      	  SolutionList = computeLadder(tempList.get(j), endWord, newPos);
+      	  SolutionList = computeLadder(tempList.get(j), endWord, newPos); // RECURSIVELY CALL computeLadder to find the next word in the ladder.
       	  if (SolutionList.get(SolutionList.size() - 1).equals(endWord))
       	  {
-      		  return SolutionList;
+      		  return SolutionList; // If the last word is the endWord, we have found the solution!
       	  }
         }
-        //SolutionList.remove(SolutionList.indexOf(startWord));
-        checkedList.add(startWord);//keep a list of words we have already seen
+        checkedList.add(startWord); //keep a list of words we have already seen
         SolutionList.remove(startWord);
+        if (SolutionList.isEmpty()) // Throw exception IF no word ladder can be found. Program reaches here if startWord has been removed.
+        {
+      	  throw new NoSuchLadderException("No such ladder exists between words \'" + startWord + "\' and \'" + endWord + "\'.");
+        }
         return SolutionList;
     }
 
@@ -134,6 +216,4 @@ public class WordLadderSolver implements Assignment4Interface
     {
         throw new UnsupportedOperationException("Not implemented yet!");
     }
-
-    // add additional methods here
 }
